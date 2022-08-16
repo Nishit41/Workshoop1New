063e902dc7c770468542be27bb0a7bfe3e3147e8
Nishit <nishitranjan41@gmail.com>
2022-08-16 18:57:36 +0530

    this is about creating a board

---
+++ src/com/bridgelab/tictactoe/TicTacToeGame.java
@@ -0,0 +1,21 @@
+package com.bridgelab.tictactoe;
+
+public class TicTacToeGame {
+
+    static char[] board = new char[10];
+
+    static void createBoard(char[] board) {
+
+        for (int i = 1; i < 10; i++) {
+            board[i] = ' ';
+        }
+    }
+
+    public static void main(String[] args) {
+        createBoard(board);
+
+    }
+}
+
+
+
