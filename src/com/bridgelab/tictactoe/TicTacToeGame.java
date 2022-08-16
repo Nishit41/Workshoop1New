8ab37639a4c0cde56a1ca959de90a2054f68b4e2
Nishit <nishitranjan41@gmail.com>
2022-08-16 19:29:08 +0530

    this is about chooseletter a board

--- src/com/bridgelab/tictactoe/TicTacToeGame.java
+++ src/com/bridgelab/tictactoe/TicTacToeGame.java
@@ -1,19 +1,36 @@
 package com.bridgelab.tictactoe;
 
+import java.util.Scanner;
+
 public class TicTacToeGame {
-
-    static char[] board = new char[10];
-
+     static char computerLetter ;
     static void createBoard(char[] board) {
 
         for (int i = 1; i < 10; i++) {
             board[i] = ' ';
         }
     }
+    static void chooseLetter(){
+        System.out.println(" choose Letter X and O ");
+        Scanner sc = new Scanner(System.in);
+        char input = sc.next().charAt(0);
+        if(input == 'X')
+        {
+            computerLetter ='O';
+            System.out.println("computerLetter=>"+computerLetter);
+        }
+        else if(input == 'O'){
+         computerLetter = 'X';
+            System.out.println("computerLetter"+computerLetter);
+        }
+    }
+}
 
+class Main{
+    static char[] board = new char[10];
     public static void main(String[] args) {
-        createBoard(board);
-
+        TicTacToeGame.createBoard(board);
+        TicTacToeGame.chooseLetter();
     }
 }
 
